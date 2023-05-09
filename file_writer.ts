11dc0ba9011f4a363f806dc8efefb0bd57ad16cd
mohammad6vakili <mohammad6vakili@gmail.com>
2023-05-09 10:33:00 +0330

    feat(add rename method): :sparkles: add fs rename method

--- file_writer.ts
+++ file_writer.ts
@@ -6,13 +6,27 @@ const writeTextFile = (content: string) => {
   });
 };
 
+const writeTextFileSync = (content: string) => {
+  fs.writeFileSync("newFile.txt", content, (err: any) => {
+    console.log(err);
+  });
+};
+
 const appendTextFile = (content: string) => {
   fs.appendFile("newFile.txt", content, (err: any) => {
     console.log(err);
   });
 };
 
+const renameFile = (old_path: string, new_path: string) => {
+  fs.rename(old_path, new_path, (err: any) => {
+    console.log(err);
+  });
+};
+
 module.exports = {
   writeTextFile,
+  writeTextFileSync,
   appendTextFile,
+  renameFile,
 };
--- main.ts
+++ main.ts
@@ -4,4 +4,6 @@ const fileWriter = require("./file_writer");
 logger.sayHello("Parisa");
 
 fileWriter.writeTextFile("Hello World!");
+fileWriter.writeTextFileSync("aloooooo!");
 fileWriter.appendTextFile(" This is appended text!");
+fileWriter.renameFile("newFile.txt", "test.txt");
