fee7a520e37deab64f64818602103dde45534ef7
mohammad6vakili <mohammad6vakili@gmail.com>
2023-05-09 14:20:54 +0330

    feat(create server): :sparkles: create serve in nodejs

--- app.js
+++ app.js
@@ -1,59 +1,98 @@
-const yargs = require("yargs");
-const { addUser, listUsers, removeUser } = require("./users");
+// terminal app --------------------------------------------
+// const yargs = require("yargs");
+// const { addUser, listUsers, removeUser } = require("./users");
 
-yargs.command({
-  command: "create",
-  aliases: ["c"],
-  describe: "create new user",
-  builder: {
-    name: {
-      describe: "user name",
-      demandOption: true,
-      type: "string",
-      alias: "n",
-    },
-    mobile: {
-      describe: "user mobile",
-      demandOption: true,
-      type: "number",
-      alias: "m",
-    },
-    email: {
-      describe: "user email",
-      demandOption: true,
-      type: "string",
-      alias: "e",
-    },
-  },
-  handler: ({ name, mobile, email }) => {
-    addUser(name, mobile, email);
-  },
+// yargs.command({
+//   command: "create",
+//   aliases: ["c"],
+//   describe: "create new user",
+//   builder: {
+//     name: {
+//       describe: "user name",
+//       demandOption: true,
+//       type: "string",
+//       alias: "n",
+//     },
+//     mobile: {
+//       describe: "user mobile",
+//       demandOption: true,
+//       type: "number",
+//       alias: "m",
+//     },
+//     email: {
+//       describe: "user email",
+//       demandOption: true,
+//       type: "string",
+//       alias: "e",
+//     },
+//   },
+//   handler: ({ name, mobile, email }) => {
+//     addUser(name, mobile, email);
+//   },
+// });
+
+// yargs.command({
+//   command: "list",
+//   aliases: ["l"],
+//   describe: "list users",
+//   handler: () => {
+//     listUsers();
+//   },
+// });
+
+// yargs.command({
+//   command: "remove",
+//   aliases: ["r"],
+//   describe: "remove user from users list",
+//   builder: {
+//     name: {
+//       describe: "user name",
+//       demandOption: true,
+//       type: "string",
+//       alias: "n",
+//     },
+//   },
+//   handler: ({ name }) => {
+//     removeUser(name);
+//   },
+// });
+
+// yargs.parse();
+
+// events ----------------------------------------------------
+// const { EventEmitter } = require("events");
+
+// const ev = new EventEmitter();
+
+// ev.addListener("sayHi", (data) => {
+//   console.log("sayHi: ", data);
+// });
+
+// ev.addListener("userData", (data) => {
+//   console.log("user data: ", data);
+// });
+
+// ev.addListener("sayBye", (data) => {
+//   console.log("sayBye: ", data);
+// });
+
+// ev.emit("sayHi", "Hi");
+// ev.emit("userData", { name: "Mohammad", mobile: "09390624049" });
+// ev.emit("sayBye", "Bye");
+
+// http ------------------------------------------------------
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+  let { url, method, headers } = req;
+  console.log(`url: ${url}`);
+  console.log(`method: ${method}`);
+  console.log(`headers: ${headers}`);
+  res.write(
+    "<html><head><title>First Node Project</title></head><body><h1>hello world!</h1></body></html>"
+  );
+  res.end();
 });
 
-yargs.command({
-  command: "list",
-  aliases: ["l"],
-  describe: "list users",
-  handler: () => {
-    listUsers();
-  },
-});
-
-yargs.command({
-  command: "remove",
-  aliases: ["r"],
-  describe: "remove user from users list",
-  builder: {
-    name: {
-      describe: "user name",
-      demandOption: true,
-      type: "string",
-      alias: "n",
-    },
-  },
-  handler: ({ name }) => {
-    removeUser(name);
-  },
-});
-
-yargs.parse();
+server.listen(3000);
--- users.js
+++ users.js
@@ -1,79 +1,59 @@
-// const fs = require("fs");
-// const chalk = require("chalk");
+const fs = require("fs");
+const chalk = require("chalk");
 
-// const addUser = (name, mobile, email) => {
-//   const users = loadUsers();
-//   isUserExist = users.find((user) => user.name === name);
-//   if (isUserExist) {
-//     console.log(chalk.red("User Already Exist!"));
-//   } else {
-//     users.push({
-//       name,
-//       mobile,
-//       email,
-//     });
-//     saveUsers(users);
-//   }
-// };
+const addUser = (name, mobile, email) => {
+  const users = loadUsers();
+  isUserExist = users.find((user) => user.name === name);
+  if (isUserExist) {
+    console.log(chalk.red("User Already Exist!"));
+  } else {
+    users.push({
+      name,
+      mobile,
+      email,
+    });
+    saveUsers(users);
+  }
+};
 
-// const listUsers = () => {
-//   const users = loadUsers();
-//   if (users.length === 0) {
-//     console.log(chalk.red("There is no User!"));
-//   } else {
-//     console.table(users);
-//   }
-// };
+const listUsers = () => {
+  const users = loadUsers();
+  if (users.length === 0) {
+    console.log(chalk.red("There is no User!"));
+  } else {
+    console.table(users);
+  }
+};
 
-// const removeUser = (name) => {
-//   let users = loadUsers();
-//   isUserExist = users.find((user) => user.name === name);
-//   if (!isUserExist) {
-//     console.log(chalk.red("User doesn't exist!"));
-//   } else {
-//     let filteredUsers = users.filter((user) => user.name !== name);
-//     saveUsers(filteredUsers);
-//   }
-// };
+const removeUser = (name) => {
+  let users = loadUsers();
+  isUserExist = users.find((user) => user.name === name);
+  if (!isUserExist) {
+    console.log(chalk.red("User doesn't exist!"));
+  } else {
+    let filteredUsers = users.filter((user) => user.name !== name);
+    saveUsers(filteredUsers);
+  }
+};
 
-// const loadUsers = () => {
-//   try {
-//     const users = JSON.parse(fs.readFileSync("users.json").toString());
-//     return users;
-//   } catch (err) {
-//     console.log("contacts load error", err);
-//     return [];
-//   }
-// };
+const loadUsers = () => {
+  try {
+    const users = JSON.parse(fs.readFileSync("users.json").toString());
+    return users;
+  } catch (err) {
+    console.log("contacts load error", err);
+    return [];
+  }
+};
 
-// const saveUsers = (users) => {
-//   fs.writeFile("users.json", JSON.stringify(users), (err) => {
-//     console.log(chalk.green("User Saved Successfully!"));
-//   });
-// };
+const saveUsers = (users) => {
+  fs.writeFile("users.json", JSON.stringify(users), (err) => {
+    console.log(chalk.green("User Saved Successfully!"));
+  });
+};
 
-// module.exports = {
-//   addUser,
-//   listUsers,
-//   removeUser,
-// };
-
-const { EventEmitter } = require("events");
-
-const ev = new EventEmitter();
-
-ev.addListener("sayHi", (data) => {
-  console.log("sayHi: ", data);
-});
-
-ev.addListener("userData", (data) => {
-  console.log("user data: ", data);
-});
-
-ev.addListener("sayBye", (data) => {
-  console.log("sayBye: ", data);
-});
-
-ev.emit("sayHi", "Hi");
-ev.emit("userData", { name: "Mohammad", mobile: "09390624049" });
-ev.emit("sayBye", "Bye");
+module.exports = {
+  addUser,
+  listUsers,
+  removeUser,
+};
