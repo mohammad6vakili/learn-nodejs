29406036afaab5bf1a5cf111502931229807c705
mohammad6vakili <mohammad6vakili@gmail.com>
2023-05-11 13:35:42 +0330

    feat(add express statics): :sparkles: add express statics helper function

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const { setStatics } = require("./utils/setStatics");
+
 const app = express();
 
 // middlewares
@@ -9,6 +11,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // configure template engine
 app.set("view engine", "ejs");
 app.set("views", "views");
+
+// statics
+setStatics(app);
 
 app.listen(3000, () => {
   console.log("Server is running...");
---
+++ utils/setStatics.js
@@ -0,0 +1,17 @@
+const path = require("path");
+
+const express = require("express");
+
+const setStatics = (app) => {
+  app.use(express.static(path.join(__dirname, "../", "public")));
+  app.use(
+    express.static(
+      path.join(__dirname, "../", "node_modules", "bootstrap-v4-rtl", "dist")
+    )
+  );
+  app.use(
+    express.static(path.join(__dirname, "../", "node_modules", "font-awesome"))
+  );
+};
+
+module.exports = { setStatics };
