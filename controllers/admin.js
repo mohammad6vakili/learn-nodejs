abff436de89a8cd2a231d132e90d3512e9ca3caa
mohammad6vakili <mohammad6vakili@gmail.com>
2023-05-11 14:49:29 +0330

    feat(add controller): :sparkles: add admin controller

---
+++ controllers/admin.js
@@ -0,0 +1,10 @@
+const Todo = require("../models/todo");
+
+exports.addTodo = ({ req, res }) => {
+  if (!req.body.todo) return res.redirect("/");
+  const todo = new Todo(Math.floor(Math.random() * 1000), req.body.todo);
+  todo.save((err) => {
+    if (!err) res.redirect("/");
+    else console.log(err);
+  });
+};
