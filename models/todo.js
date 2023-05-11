44f455ea00bee9c7e78ed60424610be37c371528
mohammad6vakili <mohammad6vakili@gmail.com>
2023-05-11 14:46:28 +0330

    feat(add model): :sparkles: add todo model

---
+++ models/todo.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+const path = require("path");
+
+const rootDir = require("../utils/path");
+const filePath = path.join(rootDir, "data", "todos.json");
+
+class Todo {
+  constructor(id, text, completed = false) {
+    this.id = id;
+    this.text = text;
+    this.completed = completed;
+  }
+
+  save(callback) {
+    fs.writeFile(filePath, JSON.stringify(this), (err) => {
+      if (err) callback(err);
+    });
+  }
+}
+
+module.exports = Todo;
