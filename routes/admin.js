4622d27ede71bccd31dc4067ec3904bafa878c9e
mohammad6vakili <mohammad6vakili@gmail.com>
2023-05-12 16:08:10 +0330

    feat(add route): :sparkles: add complete todo route

--- routes/admin.js
+++ routes/admin.js
@@ -6,5 +6,6 @@ const adminController = require("../controllers/admin");
 router.post("/add-todo", adminController.addTodo);
 
 router.get("/delete-todo/:id", adminController.deleteTodo);
+router.get("/complete-todo/:id", adminController.completeTodo);
 
 module.exports = router;
