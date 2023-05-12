08799c4bd9b156ea09e8a9c952db9ea1830478cc
mohammad6vakili <mohammad6vakili@gmail.com>
2023-05-12 16:42:46 +0330

    feat(add urils): :sparkles: add todos operation helper functions

---
+++ utils/todos.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+const path = require("path");
+
+const rootDir = require("./path");
+const filePath = path.join(rootDir, "data", "todos.json");
+
+exports.getTodos = (callback) => {
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      return callback([]);
+    } else {
+      callback(JSON.parse(data));
+    }
+  });
+};
+
+exports.saveTodos = (todos, callback) => {
+  fs.writeFile(filePath, JSON.stringify(todos), (err) => {
+    callback(err);
+  });
+};
+
+exports.generateRandomId = () => {
+  return Math.floor(Math.random() * 10000);
+};
